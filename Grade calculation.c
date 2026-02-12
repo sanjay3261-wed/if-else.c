bde1baa7e46b81741fc14297f5515e6283d26459
sanjay3261-wed <nallamuthusanjay4387@gmail.com>
2026-02-12 10:11:27 +0530

    Add files via upload

---
+++ Grade calculation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    
+    if(a>=75&&a<90) {
+      printf("Grade B");  
+    }
+   else{
+       printf("Not Grade B");
+   }
+    return 0;
+}
