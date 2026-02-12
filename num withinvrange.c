32f23c8dd4418c8de7e6c6e3808fd5724ac8321a
sanjay3261-wed <nallamuthusanjay4387@gmail.com>
2026-02-12 11:28:23 +0530

    Add files via upload

---
+++ num withinvrange.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a>=10&&a<=20) {
+      printf("Number is within range");  
+    }
+   else{
+       printf("Not number is within range");
+   }
+   
+   
+    return 0;
+}
